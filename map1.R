8831488fecdc613fb71812b927cd1020b9d1ad75
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 18:10:38 -0400

    Saving map1.R

--- map1.R
+++ map1.R
@@ -6,5 +6,5 @@ us = map_data("state")
 map1=ggplot(mergedData,aes(map_id=Row.names))
 map1=map1+geom_map(map=us,fill='white',color='black')
 map1=map1+expand_limits(x=us$long,y=us$lat)
-map1=map1+coord_map+ggtitle('USA MAP')
+map1=map1+coord_map()+ggtitle('USA MAP')
 map1
