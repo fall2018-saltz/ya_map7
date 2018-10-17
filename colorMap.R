6c7bd0403ac9657b7f8744c146135471cc51f045
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 17:57:09 -0400

    Saving colorMap.R

--- colorMap.R
+++ colorMap.R
@@ -6,4 +6,5 @@ options(scipen=999)
 colorMap=ggplot(mergedData, aes(map_id = stateName))
 colorMap=colorMap+ geom_map(map = us ,aes(fill= state.area))
 colorMap=colorMap+expand_limits(x = us$long, y = us$lat) + coord_map()
+colorMap=colorMap+ggtitle("US Color Map")
 colorMap
