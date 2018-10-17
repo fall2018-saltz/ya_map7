fcdaba7004b6c927cebf09f72020b5e3372a6afb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 17:58:42 -0400

    Saving colorMap.R

--- colorMap.R
+++ colorMap.R
@@ -2,6 +2,7 @@
 #3)	Create a color codedmap, based on the area of the state 
 
 #for ggplot visualisation we need the ggplot2 package
+#gor geom_map we need the ggmap package
 library("ggplot2")
 library("ggmap")
 us=map_data("state")
