2129de2e146ad1945c9ab7b80e213fe613379ce1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 17:34:40 -0400

    Saving colorMap.R

--- colorMap.R
+++ colorMap.R
@@ -3,7 +3,7 @@ library("ggplot2")
 library("ggmap")
 us <- map_data("state")
 options(scipen=999)
-map1 <- ggplot(clean_data_merged, aes(map_id = stateName))
-map1 <- map1 + geom_map(map = us ,aes(fill= state.area))
-map1 <- map1 + expand_limits(x = us$long, y = us$lat) + coord_map()
-map1
+colorMap=ggplot(mergedData, aes(map_id = stateName))
+colorMap=colorMap+ geom_map(map = us ,aes(fill= state.area))
+colorMap=colorMap+expand_limits(x = us$long, y = us$lat) + coord_map()
+colorMap
