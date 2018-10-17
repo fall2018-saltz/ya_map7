3a686a7772bbd4e1864f2bde128d66e40764be90
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 17:00:12 -0400

    Saving mergeData.R

--- mergeData.R
+++ mergeData.R
@@ -1,6 +1,7 @@
 
 #take the USArrests data set into arrests variable 
 arrests=USArrests
+stateName=as.vector(rownames(USArrests)
 #arrests doesn't contain a proper column name for state names but has state names as index
 #so create a column to store the state names suing rownames
 stateNamee=as.vector(rownames(USArrests))
