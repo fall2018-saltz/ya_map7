b81339242109ee7694a627d230ccc81a52c6f933
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 16:40:33 -0400

    Saving mergeData.R

--- mergeData.R
+++ mergeData.R
@@ -3,9 +3,9 @@
 arrests=USArrests
 #arrests doesn't contain a proper column name for state names but has state names as index
 #so create a column to store the state names suing rownames
-stateName=as.vector(rownames(USArrests))
-stateName=tolower(stateName)
-cbind(arrests,stateName,stringsAsFactors=FALSE)
+stateNamee=as.vector(rownames(USArrests))
+stateNamee=tolower(stateNamee)
+cbind(arrests,stateNamee,stringsAsFactors=FALSE)
 #now that we have to merge two dataframes using a common element 
 #the column element is stateName
 mergedData=merge(cleandata,arrests,by="stateName")
